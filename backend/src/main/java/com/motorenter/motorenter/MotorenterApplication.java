e197a8c593039588d33a2af0a74ab1bf6e43f361
Jakucs Viktor <jakucs42@gmail.com>
2026-08-29 20:18:02 +0200

    add basic springBoot

---
+++ backend/src/main/java/com/motorenter/motorenter/MotorenterApplication.java
@@ -0,0 +1,13 @@
+package com.motorenter.motorenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotorenterApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/motorenter/motorenter/MotorenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motorenter.motorenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
