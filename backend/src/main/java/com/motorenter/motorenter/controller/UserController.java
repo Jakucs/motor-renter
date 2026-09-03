e04356a98a39441ac34af8634ab5c4b3de2a8682
Jakucs Viktor <jakucs42@gmail.com>
2026-09-03 13:04:39 +0200

    add UserController

--- backend/src/main/java/com/motorenter/motorenter/controller/UserController.java
+++ backend/src/main/java/com/motorenter/motorenter/controller/UserController.java
@@ -1,5 +1,23 @@
 package com.motorenter.motorenter.controller;
 
+import com.motorenter.motorenter.model.User;
+import com.motorenter.motorenter.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class UserController {
-    
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user){
+        return userService.register(user);
+    }
 }
