bfbb22eb50633d1794cab454e9d7c0927ef0e12a
Jakucs Viktor <jakucs42@gmail.com>
2026-09-07 11:48:36 +0200

    add exact location

--- frontend/src/Map.jsx
+++ frontend/src/Map.jsx
@@ -1,16 +1,28 @@
 import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import { useState, useEffect } from "react";
 
 const mapContainerStyle = {
   width: "100%",
   height: "400px"
 };
 
-const center = {
+/* const center = {
   lat: 47.4979,
   lng: 19.0402
-};
+}; */
 
 function Map() {
+   const [center, setCenter] = useState({ lat: 47.4979, lng: 19.0402 });
+
+    useEffect(() => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setCenter( {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        })
+      })
+    }, [])
+
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY
   });
