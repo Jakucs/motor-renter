39cdf9edb5138f1377b2c94fe430211923d46b48
Jakucs Viktor <jakucs42@gmail.com>
2026-09-07 12:46:47 +0200

    add hamburger menu on the main page

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,14 +1,35 @@
 import Map from "./Map";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import "./Home.css";
 
 function Home() {
   const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <div className="wrapper">
-      <div id="formContent" className="fadeInDown" style={{ padding: "40px", textAlign: "center" }}>
-        <img src="moto-share.png" alt="logo" width="200" style={{ display: "block", margin: "20px auto" }} />
-        <Map></Map>
+      <div id="formContent" className="fadeInDown" style={{ padding: "40px", textAlign: "center", position: "relative" }}>
+
+        <div className="home-header">
+          <img src="moto-share.png" alt="logo" width="200" />
+          <button className="hamburger-btn" onClick={() => setMenuOpen(!menuOpen)}>
+            ☰
+          </button>
+        </div>
+
+        {menuOpen && (
+          <div className="dropdown-menu">
+            <div className="dropdown-item" onClick={() => navigate("/")}>
+              Kilépés
+            </div>
+          </div>
+        )}
+
+        <Map />
+        <br />
+        <input type="submit" className="fadeIn fourth" value="Rendelés" />
+
       </div>
     </div>
   );
