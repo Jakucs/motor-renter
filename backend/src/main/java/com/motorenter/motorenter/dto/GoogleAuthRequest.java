cc9829035f90f11d61272f0975f26699f32e9bac
Jakucs Viktor <jakucs42@gmail.com>
2026-09-09 17:59:21 +0200

    add dto

---
+++ backend/src/main/java/com/motorenter/motorenter/dto/GoogleAuthRequest.java
@@ -0,0 +1,13 @@
+package com.motorenter.motorenter.dto;
+
+public class GoogleAuthRequest {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
