b9a309dfa62cb45957d84c7c953f30433c3712d6
Jakucs Viktor <jakucs42@gmail.com>
2026-09-12 17:25:01 +0200

    add upload-picture endpoint

--- backend/src/main/java/com/motorenter/motorenter/controller/VehicleController.java
+++ backend/src/main/java/com/motorenter/motorenter/controller/VehicleController.java
@@ -3,7 +3,9 @@ package com.motorenter.motorenter.controller;
 import com.motorenter.motorenter.model.Vehicle;
 import com.motorenter.motorenter.service.VehicleService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:5173")
@@ -40,5 +42,10 @@ public class VehicleController {
     @GetMapping("/single/{id}")
     public Vehicle getVehicle(@PathVariable int id) {
         return vehicleService.getVehicleById(id);
+    }
+
+    @PostMapping("/{id}/upload-picture")
+    public Vehicle uploadPicture(@PathVariable int id, @RequestParam("file") MultipartFile file) throws IOException {
+        return vehicleService.uploadPicture(id, file);
     }
 }
