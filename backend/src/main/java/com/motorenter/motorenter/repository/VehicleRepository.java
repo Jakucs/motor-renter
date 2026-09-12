652d360ff582f836711f62a355049eb981e77cc4
Jakucs Viktor <jakucs42@gmail.com>
2026-09-12 10:48:17 +0200

    add VehicleRepository

---
+++ backend/src/main/java/com/motorenter/motorenter/repository/VehicleRepository.java
@@ -0,0 +1,10 @@
+package com.motorenter.motorenter.repository;
+
+import com.motorenter.motorenter.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface VehicleRepository extends JpaRepository {
+    List<Vehicle> findByUserId(int userId);
+}
