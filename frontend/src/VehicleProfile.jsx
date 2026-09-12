0622460bda359ee7b1f32ac18bb366359e8c5601
Jakucs Viktor <jakucs42@gmail.com>
2026-09-12 17:09:58 +0200

    load exact moto data

--- frontend/src/VehicleProfile.jsx
+++ frontend/src/VehicleProfile.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./css/VehicleProfile.css";
 
 function VehicleData() {
   const [brand, setBrand] = useState("");
