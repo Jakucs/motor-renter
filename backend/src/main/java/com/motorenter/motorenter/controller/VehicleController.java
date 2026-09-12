101c0d8b18eb286077715270593a796b2383ae8d
Jakucs Viktor <jakucs42@gmail.com>
2026-09-12 11:01:12 +0200

    add vehicleController

---
+++ backend/src/main/java/com/motorenter/motorenter/controller/VehicleController.java
@@ -0,0 +1,39 @@
+package com.motorenter.motorenter.controller;
+
+import com.motorenter.motorenter.model.Vehicle;
+import com.motorenter.motorenter.service.VehicleService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/api/vehicle")
+public class VehicleController {
+
+    private final VehicleService vehicleService;
+
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @GetMapping("/{userId}")
+    public List<Vehicle> getVehicles(@PathVariable int userId) {
+        return vehicleService.getVehiclesByUserId(userId);
+    }
+
+    @PostMapping("/{userId}")
+    public Vehicle addVehicle(@PathVariable int userId, @RequestBody Vehicle vehicle) {
+        return vehicleService.addVehicle(userId, vehicle);
+    }
+
+    @PutMapping("/{vehicleId}")
+    public Vehicle updateVehicle(@PathVariable int vehicleId, @RequestBody Vehicle updatedVehicle) {
+        return vehicleService.updateVehicle(vehicleId, updatedVehicle);
+    }
+
+    @DeleteMapping("/{vehicleId}")
+    public void deleteVehicle(@PathVariable int vehicleId) {
+        vehicleService.deleteVehicle(vehicleId);
+    }
+}
