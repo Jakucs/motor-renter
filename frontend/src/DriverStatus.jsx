a812790dafdd086bc6298d781e43144b1e5d7d5e
Jakucs Viktor <jakucs42@gmail.com>
2026-09-25 11:56:36 +0200

    add JwtFilter

--- frontend/src/DriverStatus.jsx
+++ frontend/src/DriverStatus.jsx
@@ -80,7 +80,7 @@ function DriverStatus() {
 
         try {
             const response = await authFetch(
-                `http://localhost:8080/api/profile/active`,
+                `http://localhost:8080/api/profile/${localStorage.getItem("userId")}/active`,
                 {
                     method: "PUT",
                     headers: { "Content-Type": "application/json" },
